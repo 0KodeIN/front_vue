a5ec120043381e2a8064f104cfa86b05518975b7
0Kodein <it.speed0120@gmail.com>
2021-05-22 16:14:58 +0300

    !

---
+++ src/components/Wrapper.vue
@@ -0,0 +1,25 @@
+<template>
+<section class="post">
+  {{info}}
+</section>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  name: "Wrapper",
+  data(){
+    return {
+      info: null
+    };
+
+  },
+
+  mounted() {
+    axios.get('http://127.0.0.1:8000/api/posters')
+        .then(response => {
+            this.info = response});
+  }
+
+};
+</script>
--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,36 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from 'axios'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    posts: []
   },
   mutations: {
+    SET: (state,posts)=>{
+      state.posts = posts;
+    }
   },
   actions: {
+    GET_Posts({commit}){
+      return axios('https://jsonplaceholder.typicode.com/',{
+        method: 'GET'
+      })
+          .then((posts)=>{
+            commit('SET', posts)
+            return posts
+          })
+          .catch((error)=>{
+        console.log(error)
+            return error
+      })
+    }
   },
-  modules: {
+  getters: {
+    POSTS(state){
+      return state.posts
+    }
   }
 })
