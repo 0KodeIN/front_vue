c4991b8abd80e218c82c426d3c9254438a5acb36
The1nS1ghT <makss.past@gmail.com>
2021-05-22 11:18:33 +0300

    add sign_up page

---
+++ src/components/Dialog.vue
@@ -0,0 +1,21 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name: "Dialog"
+}
+</script>
+
+<style scoped>
+
+.dialog{
+  display: flex;
+  width: 200px;
+  height: 200px;
+  background: aqua;
+}
+
+
+</style>
